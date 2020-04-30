599ad9d6d27b946a6f1621ebf32f7115c5970a05
AlexGlz <alex.glz.v@hotmail.com>
2020-04-30 03:03:28 -0500

    Hosting

---
+++ build/precache-manifest.64213034a4730ee2ffa8dd3444e385f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c84eb4c963b1f668db5f36cc202b73",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "9e1b2c08480d8dca9376",
+    "url": "/todolist/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "ce8d14117a38f96758c0",
+    "url": "/todolist/static/css/main.79689bdf.chunk.css"
+  },
+  {
+    "revision": "9e1b2c08480d8dca9376",
+    "url": "/todolist/static/js/2.5a6f3cd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.5a6f3cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8d14117a38f96758c0",
+    "url": "/todolist/static/js/main.c10f5887.chunk.js"
+  },
+  {
+    "revision": "c34286462be74df0c26a",
+    "url": "/todolist/static/js/runtime-main.3c2f5c7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
